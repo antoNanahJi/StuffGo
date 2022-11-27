a77e50e620be31452022ed64a6c1c4b9527c7938
JusttPhilip <106443209+JusttPhilip@users.noreply.github.com>
2022-11-27 16:29:13 -0500

    change of names and db

--- StuffGo/src/main/java/ctrl/Catalog.java
+++ StuffGo/src/main/java/ctrl/Catalog.java
@@ -16,13 +16,13 @@ import model.StoreModel;
  * Servlet implementation class SIS
  */
 @WebServlet("/home")
-public class Items extends HttpServlet {
+public class Catalog extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
-	public Items() {
+	public Catalog() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
