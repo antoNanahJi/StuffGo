c1528c7f8b5a3393936f6d4c0847fd9611098652
philipn1 <56880827+philipn1@users.noreply.github.com>
2022-12-15 16:01:46 -0500

    login checkup fix

--- StuffGo/src/main/webapp/scripts/checkout.js
+++ StuffGo/src/main/webapp/scripts/checkout.js
@@ -44,6 +44,30 @@ function loadLogin() {
 function loadCheckout() {
 	document.getElementById("checkout-login").style.display = "none";
 	document.getElementById("checkout").style.display = "block";
+	console.log('running on load');
+	// In the navbar we have a login button, this button should be logout if the user is logged in
+	const loginText = document.getElementById('loginout');
+	// In the navbar we have a name, this name should be hidden if the user is not logged in
+	const nameNav = document.getElementById('nohov');
+
+	// If the user is on the login or register page, hide the login button and the name
+	const isLoginOrRegisterPage =
+		document.URL.includes('login.jsp') || document.URL.includes('register.jsp');
+
+	if (isLoginOrRegisterPage) {
+		loginText.hidden = true;
+		nameNav.hidden = true;
+	}
+
+	// Condition for "If username inside nameNav exists (if user is logged in)"
+	if (nameNav && nameNav.innerHTML && nameNav.innerHTML.length !== 0) {
+		console.log('nameNav is not empty therefore changing login to logout');
+		// Change login button to logout button
+		loginText.innerHTML = 'logout';
+		// Replace to logoutpage
+		loginText.href = loginText.href.replace('login.jsp', 'logout.jsp');
+		console.log(loginText.href);
+	}
 }
 
 // Adds a toast
